461dc2f5b4654512d223ee597dbda9c973a32073
Ayende Rahien <ayende@ayende.com>
2011-09-21 09:40:37 +0300

    Edited Orev/Helpers/RavenHelpers.cs via GitHub

--- Orev/Helpers/RavenHelpers.cs
+++ Orev/Helpers/RavenHelpers.cs
@@ -10,6 +10,8 @@ namespace Orev.Helpers
 	{
 		public static int ToIntId(this string id)
 		{
+			// I assume you did that to avoid MVC routing issues, there are better ways
+			// http://weblogs.asp.net/shijuvarghese/archive/2010/06/04/how-to-work-ravendb-id-with-asp-net-mvc-routes.aspx
 			return int.Parse(id.Substring(id.LastIndexOf('/') + 1));
 		}
 	}
